5375a554ab4b5edf3345026451545ee035872811
Purna Shrestha <crush9918@gmail.com>
2025-01-03 23:54:38 +0545

    feat: Add the dynamic ride bar

--- src/components/RideBar.tsx
+++ src/components/RideBar.tsx
@@ -1,7 +1,11 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { TbMapPin, TbBrandHipchat, TbUser } from 'react-icons/tb';
 
-const RideBar = () => {
+interface RideBarProps {
+  fromHome?: boolean;
+}
+
+const RideBar: React.FC<RideBarProps> = ({ fromHome = false }) => {
   const [showRideBar, setShowRideBar] = useState(false);
 
   useEffect(() => {
@@ -28,40 +32,46 @@ const RideBar = () => {
 
   return (
     <main
-      className={`fixed bottom-0 z-40 w-full bg-none py-0 transition-all duration-500 ease-in-out ${window.scrollY > 0 ? 'py-0' : 'px-6'} ${showRideBar ? 'translate-y-0' : 'translate-y-20'} `}
+      className={`${fromHome ? `fixed bottom-0 z-40 w-full bg-none py-0 transition-all duration-500 ease-in-out ${window.scrollY > 0 ? 'py-0' : 'px-6'} ${showRideBar ? 'translate-y-0' : 'translate-y-20'} ` : `mt-24 p-0`}`}
     >
       <form
         action=""
         className="flex items-center justify-between gap-2 rounded-full border bg-white p-2 shadow"
       >
         <div className="inline-flex w-full items-center rounded-full bg-teal-100">
-          <label htmlFor="from" className="inline-flex items-center gap-2 pl-4">
+          <label
+            htmlFor="from"
+            className="inline-flex items-center gap-2 pl-4 text-sm"
+          >
             <TbMapPin className="text-lg" />
             From
           </label>
           <input
             type="text"
             id="from"
-            className="w-full rounded-full bg-transparent px-2 py-3 text-dark ring-inset placeholder:text-dark focus:ring-1 focus:ring-teal-600"
+            className="w-full rounded-full bg-transparent px-2 py-3 text-sm text-dark ring-inset placeholder:text-dark/50 focus:ring-1 focus:ring-teal-600"
             placeholder="Current Location"
           />
         </div>
         <div className="inline-flex w-full items-center rounded-full bg-teal-100">
-          <label htmlFor="to" className="inline-flex items-center gap-2 pl-4">
+          <label
+            htmlFor="to"
+            className="inline-flex items-center gap-2 pl-4 text-sm"
+          >
             <TbMapPin className="text-lg" />
             To
           </label>
           <input
             type="text"
             id="to"
-            className="w-full rounded-full bg-transparent px-2 py-3 text-dark ring-inset placeholder:text-dark focus:ring-1 focus:ring-teal-600"
+            className="w-full rounded-full bg-transparent px-2 py-3 text-sm text-dark ring-inset placeholder:text-dark/50 focus:ring-1 focus:ring-teal-600"
             placeholder="Kathmandu BernHardt College"
           />
         </div>
         <div className="inline-flex w-full items-center rounded-full bg-teal-100">
           <label
             htmlFor="message"
-            className="inline-flex items-center gap-2 pl-4"
+            className="inline-flex items-center gap-2 pl-4 text-sm"
           >
             <TbBrandHipchat className="text-lg" />
             Message
@@ -69,21 +79,21 @@ const RideBar = () => {
           <input
             type="text"
             id="message"
-            className="w-full rounded-full bg-transparent px-2 py-3 text-dark ring-inset placeholder:text-dark focus:ring-1 focus:ring-teal-600"
-            placeholder="Optional message"
+            className="w-full rounded-full bg-transparent px-2 py-3 text-sm text-dark ring-inset placeholder:text-dark/50 focus:ring-1 focus:ring-teal-600"
+            placeholder="I'm leaving in 5 minutes"
           />
         </div>
         <div className="inline-flex w-full items-center rounded-full bg-teal-100">
           <label
             htmlFor="role"
-            className="inline-flex min-w-20 items-center gap-2 pl-4"
+            className="inline-flex min-w-20 items-center gap-2 pl-4 text-sm"
           >
             <TbUser className="text-lg" />
             I'm a
           </label>
           <select
             id="role"
-            className="mr-2 w-full rounded-full bg-transparent px-2 py-3 text-dark ring-inset placeholder:text-dark focus:ring-1 focus:ring-teal-600"
+            className="mr-2 w-full rounded-full bg-transparent px-2 py-3 text-sm text-dark ring-inset placeholder:text-dark/50 focus:ring-1 focus:ring-teal-600"
           >
             <option value="driver">Rider</option>
             <option value="passenger">Passenger</option>
@@ -91,7 +101,7 @@ const RideBar = () => {
         </div>
         <button
           type="submit"
-          className="inline-flex items-center gap-2 rounded-full bg-teal-300 px-4 py-3"
+          className="inline-flex items-center gap-2 rounded-full bg-teal-300 px-6 py-3 text-sm"
         >
           {/* <TbSearch className="text-lg" /> */}
           Confirm
--- src/pages/RiderPage.tsx
+++ src/pages/RiderPage.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import RideBar from '../components/RideBar';
+import Title from '../components/ui/Title';
 
 const RiderPage = () => {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <Title
+        title="Post a ride & Make an Impact"
+        description="Share your ride with your co-workers and students sharing the same route and utilize the resources and empty seats of your vehicle. Save money, time and the environment. Your ride can make a difference. Share your ride now & be a hero!"
+      />
+      <RideBar />
+    </main>
   );
-}
+};
 
 export default RiderPage;
